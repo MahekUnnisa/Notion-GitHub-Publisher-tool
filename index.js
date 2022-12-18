875fb9eedeeebf409b10cd5c5ed7e5a5ba3df318
Mahek Unnisa <mahekunnisa786@gmail.com>
2022-12-19 00:01:34 +0530

    Commented

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const notion = new Client({
     auth: notion_key,
     notionVersion:'2022-06-28',
 })
-
+//retrieve the database
 const getdata = async ()=> {
     const response = await notion.databases.retrieve({ database_id: databaseId });
     console.log(response);
