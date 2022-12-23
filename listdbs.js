db929ef0f9c33b036651cf459a82e26e589eadb9
Mahek Unnisa <mahekunnisa786@gmail.com>
2022-12-24 00:20:53 +0530

    Fetched pageIDs with a loop

--- listdbs.js
+++ listdbs.js
@@ -18,8 +18,8 @@ const getHeadPageId = async () => {
         const pageID = response.parent.page_id
         console.log(pageID)
 }
-
-const getdata = async () => {
+//gets list of all the databases which are shared by the integration
+const getdbs = async () => {
     const response = await notion.databases.query({
       database_id: databaseId,
       sorts: [
@@ -31,9 +31,10 @@ const getdata = async () => {
     });
     console.log(response);
 }
-// getdata()
+// getdbs()
 // getHeadPageId();
 const getchildren = async () => {
+  //ismstaed of block id you can use page id to retrieve the llist of children
     const blockId = '5dd7cc77-a640-47a2-8c2e-936c6c138ac6';
     const response = await notion.blocks.children.list({
       block_id: blockId,
@@ -41,12 +42,27 @@ const getchildren = async () => {
     });
     console.log(response);
   }
+  //to get child blocks you can recursively use same api endpoint 
   const getmorechildren = async () => {
     const blockId = '4d31e70f-71dc-47d7-ab3e-1aee4305c35b';
+    const response = await notion.blocks.children.list({
+      block_id: blockId,
+      page_size: 100,
+    });
+    const data = response.results[0].code
+    console.log(data);
+  }
+  //2a2b7f09-eb51-4ee8-b9d0-8ad7b976f785 -code block id
+  //should be used recursively
+  const getmoreandmorechildren = async () => {
+    const blockId = '2a2b7f09-eb51-4ee8-b9d0-8ad7b976f785';
     const response = await notion.blocks.children.list({
       block_id: blockId,
       page_size: 50,
     });
     console.log(response);
   }
+
+  // getchildren()
   getmorechildren()
+  // getmoreandmorechildren()
--- notionapi.js
+++ notionapi.js
@@ -2,7 +2,7 @@ const { Client } = require("@notionhq/client")
 const dotenv = require('dotenv').config()
 const notion_key = process.env.NOTION_TOKEN
 const databaseId = process.env.NOTION_DATABASE_ID
-
+const fs = require('fs');
 const notion = new Client({ 
     auth: notion_key,
     notionVersion:'2022-06-28',
@@ -11,4 +11,34 @@ const getdata = async () => {
     const response = await notion.databases.retrieve({ database_id: databaseId });
     console.log(response);
 }
-getdata()
+
+//gets list of all the databases which are shared by the integration
+const getdbs = async () => {
+    const response = await notion.databases.query({
+      database_id: databaseId,
+      sorts: [
+        {
+          property: 'Last Edited',
+          direction: 'ascending',
+        },
+      ],
+    });
+    const size = response.results.length
+    for (i = 0; i <= size-1; i++) {
+        const date = JSON.stringify(response.results[i].id)
+        fs.appendFileSync('./test2.txt', date)
+    }
+    // const data = response.results[0].id
+    // console.log(data);
+    
+
+    // const date = JSON.stringify(response)
+    // fs.writeFile('./test.json', date, (err) => {
+          
+    //     // In case of a error throw err.
+    //     if (err) throw err;
+    // })
+    // // console.log(response);
+}
+
+getdbs()
