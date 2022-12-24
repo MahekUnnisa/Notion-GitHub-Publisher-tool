a2d7f560e879d30ebca7806a0c1e497a6e1a578f
Mahek Unnisa <mahekunnisa786@gmail.com>
2022-12-24 19:35:04 +0530

    Changed to modular code

---
+++ app.js
@@ -0,0 +1,7 @@
+const getdbs = require('./getdbs');
+const getpagedetails = require('./getpages');
+const getchildren = require('./getchildren');
+
+const ids = []
+getdbs(ids)
+
---
+++ getpages.js
@@ -0,0 +1,21 @@
+const { Client } = require("@notionhq/client") 
+const dotenv = require('dotenv').config()
+const notion_key = process.env.NOTION_TOKEN
+const databaseId = process.env.NOTION_DATABASE_ID
+
+const notion = new Client({ 
+    auth: notion_key,
+    notionVersion:'2022-06-28',
+})
+
+const getpagedetails = async () => {
+    //ismstaed of block id you can use page id to retrieve the llist of children
+      const blockId = '5dd7cc77-a640-47a2-8c2e-936c6c138ac6';
+      const response = await notion.blocks.children.list({
+        block_id: blockId,
+        page_size: 50,
+      });
+      console.log(response);
+}
+
+module.exports = getpagedetails;
