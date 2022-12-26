969de3e5471db8a63cf1af95b086138f31f479af
Mahek Unnisa <mahekunnisa786@gmail.com>
2022-12-26 20:12:09 +0530

    Testing individual pages in test.js

--- getpages.js
+++ getpages.js
@@ -1,8 +1,9 @@
 const { Client } = require("@notionhq/client") 
 const dotenv = require('dotenv').config()
 const notion_key = process.env.NOTION_TOKEN
-const databaseId = process.env.NOTION_DATABASE_ID
+// const databaseId = process.env.NOTION_DATABASE_ID
 const fs = require('fs');
+const getchildren = require("./getchildren");
 const notion = new Client({ 
     auth: notion_key,
     notionVersion:'2022-06-28',
@@ -16,12 +17,25 @@ const getpagedetails = async (id) => {
       page_size: 100,
     });
     const resSize = response.results.length
+    // console.log(response.results)
     for(k=0;k<resSize;k++){
       const data = response.results[k].code
-      const res = JSON.stringify(data)
+      // const res = JSON.stringify(data)
       // fs.appendFileSync('./test2.json', data+'\n\n')
-      console.log(res)
+      // console.log(res)
       //if else blocks lagane ki zaroorat hai
+      const blockChild = response.results[0].has_children
+      if(blockChild=='true'){
+        const idOfBlock = response.results[0].id
+        getchildren(idOfBlock)
+      }
+
+      else{
+        const Codeblock = response.results[0].code
+        const codeBlock = JSON.stringify(Codeblock)
+        fs.appendFileSync('./test2.txt', codeBlock)
+      }
+      // console.log(blockChild);
     }
     
     
---
+++ test.js
@@ -0,0 +1,68 @@
+const { Client } = require("@notionhq/client") 
+const getchildren = require("./getchildren")
+const dotenv = require('dotenv').config()
+const notion_key = process.env.NOTION_TOKEN
+const databaseId = process.env.NOTION_DATABASE_ID
+const ids = []
+
+const notion = new Client({ 
+    auth: notion_key,
+    notionVersion:'2022-06-28',
+})
+
+//getdbs
+const getdbs = async () => {
+  const response = await notion.databases.query({
+    database_id: databaseId,
+    sorts: [
+      {
+        property: 'Last Edited',
+        direction: 'ascending',
+      },
+    ],
+  });
+}
+//getpage
+const getpage = async (pageId) => {
+    const response = await notion.blocks.children.list({
+      block_id: pageId,
+      page_size: 50,
+    });
+    console.log(response);
+    // if(response.results[0].has_children=='true'){
+    //     getchildren(response.results[0].id)
+    // }
+    // else{
+    //     if(response.results[0].type=='code'){
+    //         getcode()
+    //     }
+    // }
+}
+const pageId = "d3333a0d-0724-4e2a-abb2-ba6b72438e06"
+// getpage(pageId)
+//getchildren
+const retrievechildren = async (blockId) => {
+    //use this if we have has_children:true --- toggle block
+    const response = await notion.blocks.children.list({
+        block_id: blockId,
+        page_size: 50,
+      });
+      console.log("starts")
+      console.log(response);
+  }
+//retrive child
+//use when has children is false and type is code only
+const retrievechild = async(id)=>{
+    // const blockId = 'c02fc1d3-db8b-45c5-a222-27595b15aea7';
+    const response = await notion.blocks.retrieve({
+    block_id: id,
+  });
+  console.log(response.code);//check here
+}
+// const Id='d291eee7-4cfd-4387-aeda-fe7cd7715705'//contains duplicate//find dupes in an array
+// retrievechildren(Id)
+
+
+//getblock of code
+const codeId = '0847b380-dd6f-49c5-b410-f0148c7c3441'
+retrievechild(codeId)
