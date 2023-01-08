f85a3447757cd321dd3ff56335e3f6f05ae891fa
Mahek Unnisa <mahekunnisa786@gmail.com>
2023-01-08 23:41:17 +0530

    Implemented createContents from Octonode

--- static/js/githubAPI/addfile.js
+++ static/js/githubAPI/addfile.js
@@ -1,19 +1,19 @@
 var github = require('octonode');
+const fs = require('fs');
+const base64 = require('base-64');
+let github_token = process.env.GITHUB_TOKEN;
+let file = fs.readFileSync("abc.txt").toString();
+var content = base64.encode(file);
+function callback(error, result) {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(result);
+    }
+  }
 
+var client = github.client(github_token);
 // Then we instantiate a client with or without a token (as show in a later section)
+var ghrepo = client.repo('MahekUnnisa/DSAlgo-CPP');
+ghrepo.createContents('Arrays/abc', 'Added testfile', content, callback); //path
 
-var ghme           = client.me();
-var ghuser         = client.user('pksunkara');
-var ghrepo         = client.repo('pksunkara/hub');
-var ghorg          = client.org('flatiron');
-var ghissue        = client.issue('pksunkara/hub', 37);
-var ghmilestone    = client.milestone('pksunkara/hub', 37);
-var ghlabel        = client.label('pksunkara/hub', 'todo');
-var ghpr           = client.pr('pksunkara/hub', 37);
-var ghrelease      = client.release('pksunkara/hub', 37);
-var ghgist         = client.gist();
-var ghteam         = client.team(37);
-var ghproject      = client.project(37);
-var ghnotification = client.notification(37);
-
-var ghsearch = client.search();
